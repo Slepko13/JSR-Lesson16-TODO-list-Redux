17917e745b2d941729fb9e2bbf70ae4d932ef660
Mykola Slepko <slepko13@gmail.com>
2020-05-22 16:44:19 +0300

    Updates

---
+++ precache-manifest.acf4ee4ff912826b606721a841ad3fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d1f2021334f17dc1e98eaec9039b7e",
+    "url": "/JSR-Lesson16-TODO-list-Redux/index.html"
+  },
+  {
+    "revision": "fe681761b87798625d8f",
+    "url": "/JSR-Lesson16-TODO-list-Redux/static/css/main.466b8f0e.chunk.css"
+  },
+  {
+    "revision": "c0d7f5f3c72302577e21",
+    "url": "/JSR-Lesson16-TODO-list-Redux/static/js/2.42ec02a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JSR-Lesson16-TODO-list-Redux/static/js/2.42ec02a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe681761b87798625d8f",
+    "url": "/JSR-Lesson16-TODO-list-Redux/static/js/main.0c4fb5c0.chunk.js"
+  },
+  {
+    "revision": "49c9be24c5936307e1ed",
+    "url": "/JSR-Lesson16-TODO-list-Redux/static/js/runtime-main.07c9381d.js"
+  }
+]);
